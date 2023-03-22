f03aeebacb7f1197d9f6bbba2694f3ccb0c614ce
Your name <Your email>
2023-03-22 16:58:40 +0100

    morningnews frontend

---
+++ reducers/hiddenArticles.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    value: [],
+};
+export const hiddenarticleSlice = createSlice({
+    name: 'hiddenArticle',
+    initialState,
+    reducers: {
+        hidearticle: (state, action) => {
+            state.value.push(action.payload);
+        },
+        showarticle: (state, action) => {
+        state.value = []
+    },
+},
+});
+
+export const {hidearticle, showarticle} = hiddenarticleSlice.actions;
+export default hiddenarticleSlice.reducer;
+
